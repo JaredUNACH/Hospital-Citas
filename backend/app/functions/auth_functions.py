0937d0e0d11888fa961be536b153b353a3fb8455
JaredUNACH <jared.salazar65@unach.mx>
2024-09-27 03:06:02 -0600

    Directory structure backend

---
+++ backend/app/functions/auth_functions.py
@@ -0,0 +1,136 @@
+import re
+from flask import jsonify, make_response, request
+from flask_jwt_extended import create_access_token
+from ..models import db, Paciente
+from google.oauth2 import id_token
+from google.auth.transport import requests as google_requests
+
+CLIENT_ID = "577245318494-v9611dklsktb7gn5re00kce0msqh06l4.apps.googleusercontent.com"
+
+def validate_email(email):
+    email_regex = re.compile(r'^[^\s@]+@[^\s@]+\.[^\s@]+$')
+    return email_regex.match(email)
+
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not validate_email(email):
+        return jsonify({"message": "Correo electrónico inválido"}), 400
+
+    paciente = Paciente.query.filter_by(email=email).first()
+    if paciente and paciente.check_password(password):
+        access_token = create_access_token(identity={"id": paciente.id, "email": paciente.email})
+        response_data = {"message": "Inicio de sesión exitoso", "token": access_token}
+        print('Login response:', response_data)
+        response = make_response(jsonify(response_data), 200)
+        response.set_cookie('token', access_token, httponly=True, secure=True, samesite='Strict')
+        return response
+    else:
+        return jsonify({"message": "Inicio de sesión fallido. Por favor, verifica tu correo y contraseña"}), 401
+
+def register():
+    name = request.json.get('name')
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not validate_email(email):
+        return jsonify({"message": "Correo electrónico inválido"}), 400
+
+    print(f"Registering user: {name}, {email}")
+
+    user = Paciente.query.filter_by(email=email).first()
+    if user:
+        print("User already registered")
+        return jsonify({'message': 'Usuario ya registrado'}), 400
+
+    new_user = Paciente(
+        nombre=name,
+        apellido_paterno='',
+        apellido_materno='',
+        email=email,
+        fecha_nacimiento='2000-01-01'
+    )
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    access_token = create_access_token(identity={"id": new_user.id, "email": new_user.email})
+    response_data = {'message': 'Registro exitoso', 'email': email, 'name': name, 'token': access_token}
+    print('Register response:', response_data)
+    response = make_response(jsonify(response_data), 200)
+    response.set_cookie('token', access_token, httponly=True, secure=True, samesite='Strict')
+    return response
+
+def google_login():
+    token = request.json.get('credential')
+    try:
+        idinfo = id_token.verify_oauth2_token(token, google_requests.Request(), CLIENT_ID)
+
+        google_id = idinfo['sub']
+        email = idinfo['email']
+        name = idinfo['name']
+
+        if not validate_email(email):
+            return jsonify({"message": "Correo electrónico inválido"}), 400
+
+        user = Paciente.query.filter_by(email=email).first()
+        if user is None:
+            return jsonify({'message': 'Usuario no registrado'}), 400
+
+        access_token = create_access_token(identity={"id": user.id, "email": user.email})
+        response_data = {'message': 'Inicio de sesión exitoso', 'email': email, 'name': name, 'token': access_token}
+        print('Google login response:', response_data)
+        response = make_response(jsonify(response_data), 200)
+        response.set_cookie('token', access_token, httponly=True, secure=True, samesite='Strict')
+        return response
+    except ValueError:
+        return jsonify({'message': 'Error en el inicio de sesión'}), 400
+
+def google_register():
+    token = request.json.get('credential')
+    if not token:
+        print("Token is missing")
+        return jsonify({"message": "Token is missing"}), 400
+
+    try:
+        print('Received token:', token)
+        idinfo = id_token.verify_oauth2_token(token, google_requests.Request(), CLIENT_ID)
+        print('Decoded token info:', idinfo)
+
+        google_id = idinfo['sub']
+        email = idinfo['email']
+        name = idinfo['name']
+
+        if not validate_email(email):
+            print("Correo electrónico inválido")
+            return jsonify({"message": "Correo electrónico inválido"}), 400
+
+        user = Paciente.query.filter_by(email=email).first()
+        if user:
+            print(f"Usuario ya registrado con el correo: {email}")
+            return jsonify({'message': 'Usuario ya registrado'}), 400
+
+        new_user = Paciente(
+            nombre=name,
+            apellido_paterno='',
+            apellido_materno='',
+            email=email,
+            google_id=google_id,
+            fecha_nacimiento='2000-01-01'
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        access_token = create_access_token(identity={"id": new_user.id, "email": new_user.email})
+        response_data = {'message': 'Registro exitoso', 'email': email, 'name': name, 'token': access_token}
+        print('Google register response:', response_data)
+        response = make_response(jsonify(response_data), 200)
+        response.set_cookie('token', access_token, httponly=True, secure=True, samesite='Strict')
+        return response
+    except ValueError as e:
+        print('Token verification failed:', e)
+        return jsonify({'message': 'Error en el registro'}), 400
+    except Exception as e:
+        print('Error en el registro:', e)
+        return jsonify({'message': 'Error en el registro'}), 500
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,175 +1,48 @@
 from flask import Blueprint, request, jsonify, make_response
-from google.oauth2 import id_token
-from google.auth.transport import requests as google_requests  # Importa google_requests correctamente
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from flask_cors import CORS
+from .functions.auth_functions import login, register, google_login, google_register  # Importa las funciones de autenticación
 from .models import db, Paciente
-import re
 
+# Crea un Blueprint para las rutas
 routes = Blueprint('routes', __name__)
 
-# Configuración de Flask-Limiter
+# Configura el limitador de tasa para las solicitudes
 limiter = Limiter(
     get_remote_address,
-    app=None,  # No configuramos el Blueprint como una aplicación Flask
-    default_limits=["200 per day", "50 per hour"]
+    app=None,
+    default_limits=["200 per day", "50 per hour"]  # Límites por defecto: 200 por día, 50 por hora
 )
 
-CLIENT_ID = "577245318494-v9611dklsktb7gn5re00kce0msqh06l4.apps.googleusercontent.com"  # Client ID de Google
-
-def validate_email(email):
-    email_regex = re.compile(r'^[^\s@]+@[^\s@]+\.[^\s@]+$')
-    return email_regex.match(email)
-
+# Ruta para el inicio de sesión
 @routes.route('/login', methods=['POST'])
-@limiter.limit("5 per minute")
-def login():
-    email = request.json.get('email')
-    password = request.json.get('password')
+@limiter.limit("5 per minute")  # Limita a 5 solicitudes por minuto
+def login_route():
+    return login()  # Llama a la función de inicio de sesión
 
-    if not validate_email(email):
-        return jsonify({"message": "Correo electrónico inválido"}), 400
-
-    paciente = Paciente.query.filter_by(email=email).first()
-    if paciente and paciente.check_password(password):
-        access_token = create_access_token(identity={"id": paciente.id, "email": paciente.email})
-        response_data = {"message": "Inicio de sesión exitoso", "token": access_token}
-        print('Login response:', response_data)  # Verifica la respuesta del servidor
-        response = make_response(jsonify(response_data), 200)
-        response.set_cookie('token', access_token, httponly=True, secure=True, samesite='Strict')
-        return response
-    else:
-        return jsonify({"message": "Inicio de sesión fallido. Por favor, verifica tu correo y contraseña"}), 401
-
+# Ruta para el registro de usuarios
 @routes.route('/register', methods=['POST'])
-@limiter.limit("5 per minute")
-def register():
-    name = request.json.get('name')
-    email = request.json.get('email')
-    password = request.json.get('password')
+@limiter.limit("5 per minute")  # Limita a 5 solicitudes por minuto
+def register_route():
+    return register()  # Llama a la función de registro
 
-    if not validate_email(email):
-        return jsonify({"message": "Correo electrónico inválido"}), 400
-
-    print(f"Registering user: {name}, {email}")
-
-    # Verifica si el usuario ya existe
-    user = Paciente.query.filter_by(email=email).first()
-    if user:
-        print("User already registered")
-        return jsonify({'message': 'Usuario ya registrado'}), 400
-
-    # Crea un nuevo usuario
-    new_user = Paciente(
-        nombre=name,
-        apellido_paterno='',
-        apellido_materno='',
-        email=email,
-        fecha_nacimiento='2000-01-01'  
-    )
-    new_user.set_password(password)  
-    db.session.add(new_user)
-    db.session.commit()
-
-    access_token = create_access_token(identity={"id": new_user.id, "email": new_user.email})
-    response_data = {'message': 'Registro exitoso', 'email': email, 'name': name, 'token': access_token}
-    print('Register response:', response_data)  # Verifica la respuesta del servidor
-    response = make_response(jsonify(response_data), 200)
-    response.set_cookie('token', access_token, httponly=True, secure=True, samesite='Strict')
-    return response
-
+# Ruta para el inicio de sesión con Google
 @routes.route('/google-login', methods=['POST'])
-@limiter.limit("5 per minute")
-def google_login():
-    token = request.json.get('credential')
-    try:
-        idinfo = id_token.verify_oauth2_token(token, google_requests.Request(), CLIENT_ID)
+@limiter.limit("5 per minute")  # Limita a 5 solicitudes por minuto
+def google_login_route():
+    return google_login()  # Llama a la función de inicio de sesión con Google
 
-        # El token de ID es válido. Obtener el ID de la cuenta de Google del usuario desde el token decodificado.
-        google_id = idinfo['sub']
-        email = idinfo['email']
-        name = idinfo['name']
-
-        if not validate_email(email):
-            return jsonify({"message": "Correo electrónico inválido"}), 400
-
-        # Busca al usuario en la base de datos
-        user = Paciente.query.filter_by(email=email).first()
-        if user is None:
-            return jsonify({'message': 'Usuario no registrado'}), 400
-
-        # Crea un token de acceso
-        access_token = create_access_token(identity={"id": user.id, "email": user.email})
-        response_data = {'message': 'Inicio de sesión exitoso', 'email': email, 'name': name, 'token': access_token}
-        print('Google login response:', response_data)  # Verifica la respuesta del servidor
-        response = make_response(jsonify(response_data), 200)
-        response.set_cookie('token', access_token, httponly=True, secure=True, samesite='Strict')
-        return response
-    except ValueError:
-        # Token inválido
-        return jsonify({'message': 'Error en el inicio de sesión'}), 400
-
+# Ruta para el registro con Google
 @routes.route('/google-register', methods=['POST'])
-@limiter.limit("5 per minute")
-def google_register():
-    token = request.json.get('credential')
-    if not token:
-        print("Token is missing")
-        return jsonify({"message": "Token is missing"}), 400
+@limiter.limit("5 per minute")  # Limita a 5 solicitudes por minuto
+def google_register_route():
+    return google_register()  # Llama a la función de registro con Google
 
-    try:
-        print('Received token:', token)  # Verifica el token en la consola del servidor
-        idinfo = id_token.verify_oauth2_token(token, google_requests.Request(), CLIENT_ID)
-        print('Decoded token info:', idinfo)  # Verifica la información decodificada del token
-
-        # El token de ID es válido. Obtener el ID de la cuenta de Google del usuario desde el token decodificado.
-        google_id = idinfo['sub']
-        email = idinfo['email']
-        name = idinfo['name']
-
-        if not validate_email(email):
-            print("Correo electrónico inválido")
-            return jsonify({"message": "Correo electrónico inválido"}), 400
-
-        # Verifica si el usuario ya existe por correo electrónico
-        user = Paciente.query.filter_by(email=email).first()
-        if user:
-            print(f"Usuario ya registrado con el correo: {email}")
-            return jsonify({'message': 'Usuario ya registrado'}), 400
-
-        # Crea un nuevo usuario
-        new_user = Paciente(
-            nombre=name,
-            apellido_paterno='',
-            apellido_materno='',
-            email=email,
-            google_id=google_id,
-            fecha_nacimiento='2000-01-01'  
-        )
-        db.session.add(new_user)
-        db.session.commit()
-
-        # Generar un token JWT para el nuevo usuario
-        access_token = create_access_token(identity={"id": new_user.id, "email": new_user.email})
-        response_data = {'message': 'Registro exitoso', 'email': email, 'name': name, 'token': access_token}
-        print('Google register response:', response_data)  # Verifica la respuesta del servidor
-        response = make_response(jsonify(response_data), 200)
-        response.set_cookie('token', access_token, httponly=True, secure=True, samesite='Strict')
-        return response
-    except ValueError as e:
-        # Token inválido
-        print('Token verification failed:', e)  # Verifica el error en la consola del servidor
-        return jsonify({'message': 'Error en el registro'}), 400
-    except Exception as e:
-        # Otros errores
-        print('Error en el registro:', e)  # Verifica otros errores en la consola del servidor
-        return jsonify({'message': 'Error en el registro'}), 500
-
+# Ruta protegida que requiere autenticación JWT
 @routes.route('/protected', methods=['GET'])
-@jwt_required()
-@limiter.limit("10 per minute")
+@jwt_required()  # Requiere un token JWT válido
+@limiter.limit("10 per minute")  # Limita a 10 solicitudes por minuto
 def protected():
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
+    current_user = get_jwt_identity()  # Obtiene la identidad del usuario actual desde el token JWT
+    return jsonify(logged_in_as=current_user), 200  # Devuelve la identidad del usuario en la respuesta
