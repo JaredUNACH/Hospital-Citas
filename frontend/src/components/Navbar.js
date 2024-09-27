be4795971fd9f948e02beeedb97421c9889c599e
JaredUNACH <jared.salazar65@unach.mx>
2024-09-27 13:21:25 -0600

    Session destroy delete JWT

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Cambiado a useNavigate
 import "../styles/Navbar.css";
 import logo from "../images/Medical Care.svg"; // Asegúrate de ajustar la ruta al logo
 
 function Navbar() {
   const [active, setActive] = useState("nav__menu");
   const [icon, setIcon] = useState("nav__toggler");
+  const navigate = useNavigate(); // Cambiado a useNavigate
+
   const navToggle = () => {
     if (active === "nav__menu") {
       setActive("nav__menu nav__active");
@@ -15,6 +18,15 @@ function Navbar() {
       setIcon("nav__toggler toggle");
     } else setIcon("nav__toggler");
   };
+
+  const handleLogout = () => {
+    // Eliminar el token de sesión
+    localStorage.removeItem("token");
+
+    // Redirigir al usuario a la página de inicio de sesión
+    navigate("/login"); // Cambiado a navigate
+  };
+
   return (
     <nav className="nav">
       <a href="#" className="nav__brand">
@@ -32,7 +44,7 @@ function Navbar() {
           </a>
         </li>
         <li className="nav__item">
-          <a href="#" className="nav__link">
+          <a href="#" className="nav__link" onClick={handleLogout}>
             Cerrar sesión
           </a>
         </li>
