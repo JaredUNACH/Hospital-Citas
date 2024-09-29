5b38b2085620c3ceb046c4b9c49c216b2d7c34e2
JaredUNACH <jared.salazar65@unach.mx>
2024-09-28 18:59:18 -0600

    Producción

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
 
 class Config:
     # Configuración de la base de datos
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:Neymar18@localhost/citas_hospital')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://jared:zb8WfYa0iqDpopW4dVh865FQK45O8g9k@dpg-crs8kuogph6c738qude0-a.oregon-postgres.render.com/care_gzry')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Clave secreta para sesiones y JWT
-    SECRET_KEY = os.getenv('SECRET_KEY', 'Neymar18')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'af03f15da847ebb6e355fdcdc397fc4794b30b4b5f3045f4')
     
     # Configuración de cookies seguras
     SESSION_COOKIE_SECURE = True  # Asegura que las cookies solo se envíen a través de HTTPS
--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,20 @@
+from flask import Flask, send_from_directory
 from app import create_app, socketio
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Cargar variables de entorno desde el archivo .env
 
 app = create_app()
+
+# Ruta para servir los archivos estáticos de React
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists("frontend/build/" + path):
+        return send_from_directory('frontend/build', path)
+    else:
+        return send_from_directory('frontend/build', 'index.html')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
